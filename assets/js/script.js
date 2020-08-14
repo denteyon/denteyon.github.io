396c792d80b6d243f1ed6e952a0bfadb53354dd9
htetmyataung <htetmyataung2027@gmail.com>
2020-08-14 20:08:51 +0630

    Add typedjs

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+var typed = new Typed('.site-description', {
+    strings: ['Hello!', 'You can call me Tet', 'Software engineer', 'Working Hard to Evolve'],
+    smartBackspace: true,
+    loop: true,
+    loopCount: Infinity,
+    typeSpeed: 80,
+    startDelay: 100,
+    backSpeed: 80,
+    shuffle: false,
+    backDelay: 1000,
+});
